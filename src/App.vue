2ce64ec24bf7a70c382c9a3e85b1076fcfe9389e
shuisheng <2274751790@qq.com>
2023-08-08 00:02:38 +0800

    feat: 20230807-flv卡顿/safari的hls问题/websocket初步搭建

--- src/App.vue
+++ src/App.vue
@@ -1,119 +1,48 @@
 <template>
-  <div class="app-wrap">
-    <h1>rtc推流，hls拉流</h1>
-    <video
-      id="myVideo"
-      ref="videoRef"
-      muted
-      autoplay
-    ></video>
-    <div
-      id="pullVideo"
-      ref="pullVideoRef"
-    ></div>
-    <!-- <button @click="getScreen">获取窗口</button> -->
-    <button @click="startRTC">WebRTCClass</button>
-    <button @click="playHls">playHls</button>
+  <div>
+    <h1>websocket测试</h1>
+    <h2>当前SocketId：{{ ws?.socket.id }}</h2>
+    <button @click="handleSendOffer">offer</button>
   </div>
 </template>
 
 <script lang="ts" setup>
-import videoJs from 'video.js';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
-import { fetchRtcV1Publish } from './api/srs';
-import { WebRTCClass } from './network/webrtc';
+import { WebSocketClass } from '@/network/websocket';
 
-const rtc = ref<WebRTCClass>();
-const videoRef = ref<HTMLVideoElement>();
-const pullVideoRef = ref<HTMLVideoElement>();
-const liveStreamName = ref('billd/ppp');
+const ws = ref<WebSocketClass>();
 
-function playHls() {
-  const videoEl = document.createElement('video');
-  videoEl.autoplay = true;
-  videoEl.muted = true;
-  videoEl.playsInline = true;
-  videoEl.setAttribute('webkit-playsinline', 'true');
-  pullVideoRef.value?.appendChild(videoEl);
-  const videoPlayer = videoJs(videoEl, {
-    sources: [
-      {
-        src: `http://localhost:5001/${liveStreamName.value}.m3u8`,
-        type: 'application/x-mpegURL',
-      },
-    ],
+function init() {
+  ws.value = new WebSocketClass();
+  ws.value.socket.on('connect', () => {
+    console.log('connect成功', ws.value?.socket.id);
+    initReceive();
   });
-  videoPlayer
-    .play()
-    ?.then(() => {
-      console.log('播放hls成功');
-    })
-    .catch((e) => {
-      console.log('播放hls失败', e);
-    });
 }
 
-async function getScreen() {
-  const event = await navigator.mediaDevices.getDisplayMedia({
-    video: true,
-    audio: true,
+onMounted(() => {
+  init();
+});
+
+function initReceive() {
+  ws.value?.socket.on('offer', (data) => {
+    console.log('收到offer', data);
   });
-  // 视频轨、音频轨
-  // const audio = event.getAudioTracks();
-  // const video = event.getVideoTracks();
-  console.log('getDisplayMedia成功', event);
-  videoRef.value?.addEventListener('loadstart', () => {
-    console.warn('视频流-loadstart');
+  ws.value?.socket.on('answer', (data) => {
+    console.log('收到answer', data);
   });
-  videoRef.value?.addEventListener('loadedmetadata', async () => {
-    console.warn('视频流-loadedmetadata');
-    event.getTracks().forEach((track) => {
-      rtc.value?.peerConnection?.addTrack(track, event);
-    });
-    const offer = await rtc.value?.createOffer();
-    if (!offer) return;
-    await rtc.value?.setLocalDescription(offer);
-    const res = await fetchRtcV1Publish({
-      sdp: offer.sdp!,
-      liveStreamName: liveStreamName.value,
-    });
-    console.log('fetchRtcV1Publish', res.data);
-    if (res.data.code !== 200) {
-      console.error('接口返回错误');
-      return;
-    }
-    if (res.data.data.code !== 0) {
-      console.error('srs没有返回spd');
-      return;
-    }
-    await rtc.value?.setRemoteDescription(res.data.data.sdp);
+  ws.value?.socket.on('joined', (data) => {
+    console.log('收到joined', data);
   });
-  videoRef.value!.srcObject = event;
+  ws.value?.socket.on('message', (data) => {
+    console.log('收到message', data);
+  });
 }
 
-function startRTC() {
-  const webrtc = new WebRTCClass({ videoEl: videoRef.value! });
-  console.log(webrtc, 'new webrtc成功');
-  rtc.value = webrtc;
-  getScreen();
+function handleSendOffer() {
+  ws.value?.send({ msgType: 'offer', data: { sdp: 123 } });
 }
 </script>
 
-<style lang="scss" scoped>
-@import 'video.js/dist/video-js.min.css';
-
-.app-wrap {
-  #myVideo {
-    width: 700px;
-  }
-  #pullVideo {
-    width: 500px;
-    height: 600px;
-
-    :deep(video) {
-      width: 500px;
-    }
-  }
-}
-</style>
+<style lang="scss" scoped></style>
---
+++ src/network/websocket.ts
@@ -0,0 +1,39 @@
+import { Socket, io } from 'socket.io-client';
+
+export class WebSocketClass {
+  socket: Socket;
+  constructor() {
+    this.socket = io('ws://localhost:4300', {
+      transports: ['websocket'],
+      forceBase64: false,
+    });
+  }
+
+  // 发送websocket消息
+  send = ({ msgType, data }: { msgType: string; data?: any }) => {
+    if (!this.socket?.connected) {
+      console.error(
+        '【websocket】未连接成功，不发送websocket消息！',
+        msgType,
+        data
+      );
+      return;
+    }
+    console.warn('【websocket】发送消息', msgType, data);
+    const sendData = {
+      socket_id: this.socket.id,
+      data,
+    };
+    this.socket?.emit(msgType, sendData);
+  };
+
+  update = () => {
+    // this.socket=
+  };
+
+  // 手动关闭websocket连接
+  close = () => {
+    console.warn('手动关闭websocket连接', this.socket?.id);
+    this.socket?.close();
+  };
+}
